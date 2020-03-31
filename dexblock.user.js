6e3db6994631cf147defc60db812cd807111e629
AlexAMCS <32461873+AlexAMCS@users.noreply.github.com>
2020-03-31 11:40:12 -0300

    Create dexblock.user.js

---
+++ dexblock.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name     MangaDEX Anti-Fool
+// @version  1
+// @grant    none
+// @include *mangadex.org/*
+// @icon		https://mangadex.org/favicon.ico
+// @require https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js 
+// ==/UserScript==
+
+$("img").each(function() {
+  var addr = $(this).attr("src");
+	if(addr.search('aprilfools') > 0) {
+    $(this).parent().parent().hide();
+  }
+});
